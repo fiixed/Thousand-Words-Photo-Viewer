62dc52db30684be23b1ef051885ee07612e07959
Andrew Bell <fiixed@gmail.com>
2014-10-23 10:40:11 -0500

    Initial Commit

---
+++ CDThousandWords/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CDThousandWords
+//
+//  Created by Andrew Bell on 10/23/14.
+//  Copyright (c) 2014 FiixedMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
